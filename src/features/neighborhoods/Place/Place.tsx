c7e7548bfb574752a4a29aaa553fb0c53c3fb554
Divine Precious-Esue <divinepreciousesue@gmail.com>
2025-07-17 06:09:41 -0700

    Initial Place Component and Minor Design Changes

---
+++ src/features/neighborhoods/Place/Place.tsx
@@ -0,0 +1,13 @@
+import './Place.css'
+
+export default function Place() 
+{
+    return (
+        <>
+        <div className='place'>
+            <p>Bob's Burgers</p>
+            <p>116 College Drive</p>
+        </div>
+        </>
+    )
+}
