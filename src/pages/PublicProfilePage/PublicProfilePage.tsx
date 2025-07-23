cc8533cabb805feed14ec84e6870d8c084031068
Divine Precious-Esue <divinepreciousesue@gmail.com>
2025-07-23 10:10:04 -0700

    Styling

--- src/pages/PublicProfilePage/PublicProfilePage.tsx
+++ src/pages/PublicProfilePage/PublicProfilePage.tsx
@@ -11,7 +11,7 @@ export default function PublicProfilePage()
     const { id: profileId } = useParams();
 
     useScrollToTop();
-
+    //Fetch profile Data
     return (
         <>
             <div className='profile'>
