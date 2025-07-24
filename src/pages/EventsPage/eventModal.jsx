d604d7746ac524bfcbb1de57318794e2e1532234
mdou123 <ndom15304@gmail.com>
2025-07-24 12:00:43 -0700

    Individual events (#22)
    * implemented home page
    
    * package json changes
    
    * Updating event modal. Local Host is at 3001
    
    * Fixed event page But local host still on 3000
    
    * ADJUSTED URL

--- src/pages/EventsPage/eventModal.jsx
+++ src/pages/EventsPage/eventModal.jsx
@@ -1,137 +1,167 @@
 import axios from "axios";
 import { useState } from "react";
-import './EventModal.css'
+import "./EventModal.css";
+import { useAuth } from "@clerk/clerk-react";
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
 
 
 //Gets event creation info from Modal and posts it to db
-const EventModal =  ( {handleCloseModalClick}) => {
-    const [category, setCategory] =useState("")
-    const [location, setLocation] = useState("")
-    const [date, setDate] = useState("")
-    const [time, setTime] = useState('');
-    const [description, setDescription] =useState("")
+const EventModal = ({ handleCloseModalClick }) => {
+  const [category, setCategory] = useState("");
+  const [location, setLocation] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [description, setDescription] = useState("");
+  const [title, setTitle] = useState("");
 
-    const categories = [ "Food", "Music", "Sports", "Art", "None"]
+  const categories = ["Food", "Music", "Sports", "Art", "None"];
 
-    const resetModal =() =>{
-        setCategory('')
-        setLocation('')
-        setDate('')
-        setTimeInput('')
-        setDescription('')
+  const resetModal = () => {
+    setCategory("");
+    setLocation("");
+    setDate("");
+    setTime("");
+    setDescription("");
+    setTitle("");
+  };
+
+  const { isLoaded, userId: clerkId, getToken } = useAuth();
+
+  //This method will be called onclick to send user event data to the backend
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    const combinedDateTime = `${date}T${time}`;
+
+    const eventData = {
+      title: title,
+      category: category,
+      location: location,
+      dateTime: combinedDateTime,
+      description: description,
+    };
+    console.log(combinedDateTime);
+    console.log(clerkId);
+
+
+    try {
+      const token = await getToken();
+      console.log("Hello");
+      console.log(eventData)
+      await axios.post(`${BACKEND_URL}/events`, eventData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log("goodbye");
+      handleCloseModalClick();
+      resetModal();
+    } catch (error) {
+      console.error("Issue creating Event", error);
     }
-    
-    //This method will be called onclick to send user event data to the backend
-    const handleFormSubmit = async (event)=>{    
-        event.preventDefault()
-        
-        const combinedDateTime = `${date}T${time}`;
+  };
 
-        const eventData = {
-            category: category,
-            location: location,
-            dateTime: combinedDateTime,
-            description: description
-        }
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <h2>Create New Event</h2>
+        <form onSubmit={handleFormSubmit}>
+          <div className="form-group">
+            <label>Event Title:</label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              required
+            />
+          </div>
+          {/* Location */}
 
-        try{
-            console.log("Hello")
-            await axios.post(`${BACKEND_URL}/events`, eventData)
-            handleCloseModalClick()
-            resetModal()
-        }catch(error){
-            console.error("Issue creating Event", error)
-        }}
+          <div className="form-group">
+            <label>Location:</label>
+            <input
+              type="text"
+              id="location"
+              name="location"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+              required
+            />
+          </div>
+          {/* Description */}
+          <div className="form-group">
+            <label>Description</label>
+            <textarea
+              id="description"
+              name="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              required
+            ></textarea>
+          </div>
 
-    return(
-        <div className="modal-overlay">
-            <div className="modal-content">
-                <h2>Create New Event</h2>
-                <form onSubmit={handleFormSubmit}>
-                    {/* Location */}
-
-                    <div className="form-group">
-                        <label>Location:</label>
-                        <input 
-                            type="text" 
-                            id="location"
-                            name="location"
-                            value={location}
-                            onChange={(e) => setLocation(e.target.value)}
-                            required
-                        />
-                    </div>
-                    {/* Description */}
-                    <div className="form-group">
-                        <label>Description</label>
-                        <textarea
-                            id="description"
-                            name="description"
-                            value={description}
-                            onChange={(e) => setDescription(e.target.value)}
-                            required
-                        >
-                        </textarea>
-                    </div>
-
-                    {/* Category button selection */}
-                    <div className="form-group">
-                        <label>Categories:</label>
-                        <div className="category-buttons-group">
-                            {categories.map((cat)=>(
-                            <button
-                            type="button"
-                            className={category === cat ? "is-active" : ""}
-                            key={cat}
-                                onClick={() => {
-                                console.log("Category clicked:", cat);
-                                setCategory(cat);
-                                }}>
-                                {cat}
-                            </button>
-                            
-                            ))}
-                        </div>
-                    </div>
-                {/* Date of Event */}
-                <div className="form-group">
-                    <label>Event Date: </label>
-                    <input
-                    type="date" 
-                    id="eventDate"
-                    name="eventDate"
-                    value={date}
-                    onChange={(e) => setDate(e.target.value)}
-                    required
-                    />
-                </div>
-                {/* TIme of Event */}
-                <div className="form-group">
-                    <label> Event Time:</label>
-                    <input
-                        type="time" 
-                        id="eventTime"
-                        name="eventTime"
-                        value={time}
-                        onChange={(e) => setTime(e.target.value)}
-                        required
-                    />
-                </div>
-                {/* Submit/Clear Button */}
-                    <div className="form-actions">
-                        <button type="submit" className="submit-button">Create Event</button>
-                        <button type="button" className="cancel-button" onClick={handleCloseModalClick}>Cancel</button>
-                    </div>
-
-                </form>
+          {/* Category button selection */}
+          <div className="form-group">
+            <label>Categories:</label>
+            <div className="category-buttons-group">
+              {categories.map((cat) => (
+                <button
+                  type="button"
+                  className={category === cat ? "is-active" : ""}
+                  key={cat}
+                  onClick={() => {
+                    console.log("Category clicked:", cat);
+                    setCategory(cat);
+                  }}
+                >
+                  {cat}
+                </button>
+              ))}
             </div>
-        </div>
+          </div>
+          {/* Date of Event */}
+          <div className="form-group">
+            <label>Event Date: </label>
+            <input
+              type="date"
+              id="eventDate"
+              name="eventDate"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              required
+            />
+          </div>
+          {/* TIme of Event */}
+          <div className="form-group">
+            <label> Event Time:</label>
+            <input
+              type="time"
+              id="eventTime"
+              name="eventTime"
+              value={time}
+              onChange={(e) => setTime(e.target.value)}
+              required
+            />
+          </div>
+          {/* Submit/Clear Button */}
+          <div className="form-actions">
+            <button type="submit" className="submit-button">
+              Create Event
+            </button>
+            <button
+              type="button"
+              className="cancel-button"
+              onClick={handleCloseModalClick}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-    )
-
-
-    
-}
-
-export default EventModal
+export default EventModal;
