a3c14dbbe16700b0b6629b5d3bfc383453e489f9
Divine Precious-Esue <divinepreciousesue@gmail.com>
2025-07-25 15:42:36 -0700

    Deployment

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-react'
+import { CircularProgress } from '@mui/material'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+const loadingScreen = <div style={{ textAlign: 'center' }}><CircularProgress /></div>;
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
@@ -28,7 +31,7 @@ const theme = createTheme({
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <ClerkLoaded>
+     <ClerkLoaded fallback={loadingScreen as any}>
         <ThemeProvider theme={theme}>
           <App />
         </ThemeProvider>
