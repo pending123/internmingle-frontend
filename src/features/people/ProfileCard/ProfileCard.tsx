05f8dcd22c02c7a46579e842a43729efc8bf9545
Divine <155365475+divineprecious@users.noreply.github.com>
2025-07-28 13:21:50 -0700

    Search by Company, Reset Filters, Loading Neighborhood Data (#28)
    * Added Basic Loading State
    
    * Added Search by Company and Loading State on Neighborhood Page

--- src/features/people/ProfileCard/ProfileCard.tsx
+++ src/features/people/ProfileCard/ProfileCard.tsx
@@ -36,6 +36,7 @@ export default function ProfileCard({ profile }: ProfileCardProps)
             <div className="profilePhoto"></div>
             <h2>{profile.firstName}</h2>
             <h4>{profile.workPosition} • {profile.company}</h4>
+            <p className='profileDesc'>{profile.bio}</p>
             <ul className='profileTags'>
             {tags.map((tag, index) => (
                 <li key={index}>{tag}</li>
