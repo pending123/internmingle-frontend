925aaafe5c8f0861653d8f461b3f96f7dfd14669
Mamadou Ndom <ndom15304@gmail.com>
2025-07-28 16:35:30 -0700

    Fixed  individual Profile Page styling

--- src/pages/EventsPage/EventsPage.jsx
+++ src/pages/EventsPage/EventsPage.jsx
@@ -80,10 +80,6 @@ const eventsPage = () => {
     setShowModal(false);
   };
 
-  useEffect(() => {
-    console.log("Events state updated in frontend:", events); // <--- What does this show?
-}, [events]);
-
   return (
     <Container maxWidth="lg" sx={{ py: 4 }}>
       {/* Header Section */}
