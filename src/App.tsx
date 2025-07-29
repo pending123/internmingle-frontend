208380c97dea91fc59cf56f73ecdb31a9b221707
Divine <155365475+divineprecious@users.noreply.github.com>
2025-07-29 09:18:03 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -140,7 +140,7 @@ function App() {
             path="/profiles/me" 
             element={
               <ProtectedRoute>
-                <PersonalProfilePage />
+                <PublicProfile />
               </ProtectedRoute>
             }
           />
